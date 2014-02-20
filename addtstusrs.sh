6ab9f465e47ef4e02b75b2c965c1fe2f8e810d6e
sa-swedel <swedel@summitanalytical.com>
2014-02-19 16:15:56 -0800

    various cosmetic edits

--- addtstusrs.sh
+++ addtstusrs.sh
@@ -4,45 +4,45 @@
 # user roles
 
 # CRC
-declare -a scsite1=(scsite1 928a36be170e2ff2821953535755abfe c48e609aff1d7581a0e7db90a4e42a12 scone@studyspot.com SC ONE 1 1 "2013-10-17 17:52:33" "2014-01-21 12:17:22" "2013-10-18 13:53:50" 0 GMT 0.0 0)
-declare -a scsite2=(scsite2 928a36be170e2ff2821953535755abfe c48e609aff1d7581a0e7db90a4e42a12 sctwo@studyspot.com SC TWO 1 1 "2013-10-17 17:52:33" "2014-01-21 12:17:22" "2013-10-18 13:53:50" 0 GMT 0.0 0)
-declare -a scsite123=(scsite123 928a36be170e2ff2821953535755abfe c48e609aff1d7581a0e7db90a4e42a12 sc123@studyspot.com SC 123 1 1 "2013-10-17 17:52:33" "2014-01-21 12:17:22" "2013-10-18 13:53:50" 0 GMT 0.0 0)
-declare -a scsite23=(scsite23 928a36be170e2ff2821953535755abfe c48e609aff1d7581a0e7db90a4e42a12 sc2and3@studyspot.com SC 23 1 1 "2013-10-17 17:52:33" "2014-01-21 12:17:22" "2013-10-18 13:53:50" 0 GMT 0.0 0)
+declare -a scsite1=(scsite1 928a36be170e2ff2821953535755abfe c48e609aff1d7581a0e7db90a4e42a12 scone@studyspot.com SC Site1 1 1 "2013-10-17 17:52:33" "2014-01-21 12:17:22" "2013-10-18 13:53:50" 0 GMT 0.0 0)
+declare -a scsite2=(scsite2 928a36be170e2ff2821953535755abfe c48e609aff1d7581a0e7db90a4e42a12 sctwo@studyspot.com SC Site2 1 1 "2013-10-17 17:52:33" "2014-01-21 12:17:22" "2013-10-18 13:53:50" 0 GMT 0.0 0)
+declare -a scsite123=(scsite123 928a36be170e2ff2821953535755abfe c48e609aff1d7581a0e7db90a4e42a12 sc123@studyspot.com SC Site1,2,3 1 1 "2013-10-17 17:52:33" "2014-01-21 12:17:22" "2013-10-18 13:53:50" 0 GMT 0.0 0)
+declare -a scsite23=(scsite23 928a36be170e2ff2821953535755abfe c48e609aff1d7581a0e7db90a4e42a12 sc2and3@studyspot.com SC Site2,3 1 1 "2013-10-17 17:52:33" "2014-01-21 12:17:22" "2013-10-18 13:53:50" 0 GMT 0.0 0)
 
 # INV
-declare -a invsite1=(itest1 4fa36550db39bcbd10cc18323046cc1e d3e05456a528820e931ff664692be7cb ione@studyspot.com INV ONE 1 1 "2013-10-18 13:57:08" "2013-10-18 13:57:08" "2013-10-18 13:57:08" 0 America/Denver -6.0 0)
-declare -a invsite2=(itest2 4fa36550db39bcbd10cc18323046cc1e d3e05456a528820e931ff664692be7cb itwo@studyspot.com INV TWO 1 1 "2013-10-18 13:57:08" "2013-10-18 13:57:08" "2013-10-18 13:57:08" 0 America/Denver -6.0 0)
-declare -a invsite123=(itest3 4fa36550db39bcbd10cc18323046cc1e d3e05456a528820e931ff664692be7cb i123@studyspot.com INV 123 1 1 "2013-10-18 13:57:08" "2013-10-18 13:57:08" "2013-10-18 13:57:08" 0 America/Denver -6.0 0)
-declare -a invsite23=(itest4 4fa36550db39bcbd10cc18323046cc1e d3e05456a528820e931ff664692be7cb i2and3@studyspot.com INV 23 1 1 "2013-10-18 13:57:08" "2013-10-18 13:57:08" "2013-10-18 13:57:08" 0 America/Denver -6.0 0)
+declare -a invsite1=(itest1 4fa36550db39bcbd10cc18323046cc1e d3e05456a528820e931ff664692be7cb ione@studyspot.com Inv Site1 1 1 "2013-10-18 13:57:08" "2013-10-18 13:57:08" "2013-10-18 13:57:08" 0 America/Denver -6.0 0)
+declare -a invsite2=(itest2 4fa36550db39bcbd10cc18323046cc1e d3e05456a528820e931ff664692be7cb itwo@studyspot.com Inv Site2 1 1 "2013-10-18 13:57:08" "2013-10-18 13:57:08" "2013-10-18 13:57:08" 0 America/Denver -6.0 0)
+declare -a invsite123=(itest3 4fa36550db39bcbd10cc18323046cc1e d3e05456a528820e931ff664692be7cb i123@studyspot.com Inv Site1,2,3 1 1 "2013-10-18 13:57:08" "2013-10-18 13:57:08" "2013-10-18 13:57:08" 0 America/Denver -6.0 0)
+declare -a invsite23=(itest4 4fa36550db39bcbd10cc18323046cc1e d3e05456a528820e931ff664692be7cb i2and3@studyspot.com Inv Site2,3 1 1 "2013-10-18 13:57:08" "2013-10-18 13:57:08" "2013-10-18 13:57:08" 0 America/Denver -6.0 0)
 
 # DM
-declare -a dmsite1=(dmsite1 ad3db225069f7781b3b4f9c4a9ac5b18 704e06f50cf31f89c5d8925c91d6a9eb dmone@studyspot.com DM ONE 1 1 "2013-10-18 15:10:59" "2013-10-18 15:10:59" "2013-10-18 15:10:59" 0 America/Denver -6.0 0)
-declare -a dmsite2=(dmsite2 ad3db225069f7781b3b4f9c4a9ac5b18 704e06f50cf31f89c5d8925c91d6a9eb dmtwo@studyspot.com DM TWO 1 1 "2013-10-18 15:10:59" "2013-10-18 15:10:59" "2013-10-18 15:10:59" 0 America/Denver -6.0 0)
-declare -a dmsite123=(dmsite123 ad3db225069f7781b3b4f9c4a9ac5b18 704e06f50cf31f89c5d8925c91d6a9eb dm123@studyspot.com DM 123 1 1 "2013-10-18 15:10:59" "2013-10-18 15:10:59" "2013-10-18 15:10:59" 0 America/Denver -6.0 0)
-declare -a dmsite23=(dmsite23 ad3db225069f7781b3b4f9c4a9ac5b18 704e06f50cf31f89c5d8925c91d6a9eb dm2and3@studyspot.com DM 23 1 1 "2013-10-18 15:10:59" "2013-10-18 15:10:59" "2013-10-18 15:10:59" 0 America/Denver -6.0 0)
+declare -a dmsite1=(dmsite1 ad3db225069f7781b3b4f9c4a9ac5b18 704e06f50cf31f89c5d8925c91d6a9eb dmone@studyspot.com DM Site1 1 1 "2013-10-18 15:10:59" "2013-10-18 15:10:59" "2013-10-18 15:10:59" 0 America/Denver -6.0 0)
+declare -a dmsite2=(dmsite2 ad3db225069f7781b3b4f9c4a9ac5b18 704e06f50cf31f89c5d8925c91d6a9eb dmtwo@studyspot.com DM Site2 1 1 "2013-10-18 15:10:59" "2013-10-18 15:10:59" "2013-10-18 15:10:59" 0 America/Denver -6.0 0)
+declare -a dmsite123=(dmsite123 ad3db225069f7781b3b4f9c4a9ac5b18 704e06f50cf31f89c5d8925c91d6a9eb dm123@studyspot.com DM Site1,2,3 1 1 "2013-10-18 15:10:59" "2013-10-18 15:10:59" "2013-10-18 15:10:59" 0 America/Denver -6.0 0)
+declare -a dmsite23=(dmsite23 ad3db225069f7781b3b4f9c4a9ac5b18 704e06f50cf31f89c5d8925c91d6a9eb dm2and3@studyspot.com DM Site2,3 1 1 "2013-10-18 15:10:59" "2013-10-18 15:10:59" "2013-10-18 15:10:59" 0 America/Denver -6.0 0)
 
 # MON
-declare -a monsite1=(mtest1 6f80f1f624edbb358ac2294739f02165 635fd9951cc5ae655f16b2bb0563fcad mone@studyspot.com MON ONE 1 1 "2013-10-18 15:14:29" "2013-10-18 15:14:29" "2013-10-18 15:14:29" 0 America/Denver -6.0 0)
-declare -a monsite2=(mtest2 6f80f1f624edbb358ac2294739f02165 635fd9951cc5ae655f16b2bb0563fcad mtwo@studyspot.com MON TWO 1 1 "2013-10-18 15:14:29" "2013-10-18 15:14:29" "2013-10-18 15:14:29" 0 America/Denver -6.0 0)
-declare -a monsite123=(mtest3 6f80f1f624edbb358ac2294739f02165 635fd9951cc5ae655f16b2bb0563fcad m123@studyspot.com MON 123 1 1 "2013-10-18 15:14:29" "2013-10-18 15:14:29" "2013-10-18 15:14:29" 0 America/Denver -6.0 0)
-declare -a monsite23=(mtest4 6f80f1f624edbb358ac2294739f02165 635fd9951cc5ae655f16b2bb0563fcad m2and3@studyspot.com MON 23 1 1 "2013-10-18 15:14:29" "2013-10-18 15:14:29" "2013-10-18 15:14:29" 0 America/Denver -6.0 0)
+declare -a monsite1=(mtest1 6f80f1f624edbb358ac2294739f02165 635fd9951cc5ae655f16b2bb0563fcad mone@studyspot.com Mon Site1 1 1 "2013-10-18 15:14:29" "2013-10-18 15:14:29" "2013-10-18 15:14:29" 0 America/Denver -6.0 0)
+declare -a monsite2=(mtest2 6f80f1f624edbb358ac2294739f02165 635fd9951cc5ae655f16b2bb0563fcad mtwo@studyspot.com Mon Site2 1 1 "2013-10-18 15:14:29" "2013-10-18 15:14:29" "2013-10-18 15:14:29" 0 America/Denver -6.0 0)
+declare -a monsite123=(mtest3 6f80f1f624edbb358ac2294739f02165 635fd9951cc5ae655f16b2bb0563fcad m123@studyspot.com Mon Site1,2,3 1 1 "2013-10-18 15:14:29" "2013-10-18 15:14:29" "2013-10-18 15:14:29" 0 America/Denver -6.0 0)
+declare -a monsite23=(mtest4 6f80f1f624edbb358ac2294739f02165 635fd9951cc5ae655f16b2bb0563fcad m2and3@studyspot.com Mon Site2,3 1 1 "2013-10-18 15:14:29" "2013-10-18 15:14:29" "2013-10-18 15:14:29" 0 America/Denver -6.0 0)
 
 # SPON
-declare -a sponsite1=(stest1 ff9d86cea1b021e9409657eb94c91860 8c4f98705d08c92f163ad3da7ece9f77 sone@studyspot.com SPON ONE 1 1 "2013-10-18 15:17:41" "2013-10-18 15:17:41" "2013-10-18 15:17:41" 0 America/Denver -6.0 0)
-declare -a sponsite2=(stest2 ff9d86cea1b021e9409657eb94c91860 8c4f98705d08c92f163ad3da7ece9f77 stwo@studyspot.com SPON TWO 1 1 "2013-10-18 15:17:41" "2013-10-18 15:17:41" "2013-10-18 15:17:41" 0 America/Denver -6.0 0)
-declare -a sponsite123=(stest3 ff9d86cea1b021e9409657eb94c91860 8c4f98705d08c92f163ad3da7ece9f77 s123@studyspot.com SPON 123 1 1 "2013-10-18 15:17:41" "2013-10-18 15:17:41" "2013-10-18 15:17:41" 0 America/Denver -6.0 0)
-declare -a sponsite23=(stest4 ff9d86cea1b021e9409657eb94c91860 8c4f98705d08c92f163ad3da7ece9f77 s2and3@studyspot.com SPON 23 1 1 "2013-10-18 15:17:41" "2013-10-18 15:17:41" "2013-10-18 15:17:41" 0 America/Denver -6.0 0)
+declare -a sponsite1=(stest1 ff9d86cea1b021e9409657eb94c91860 8c4f98705d08c92f163ad3da7ece9f77 sone@studyspot.com Spon Site1 1 1 "2013-10-18 15:17:41" "2013-10-18 15:17:41" "2013-10-18 15:17:41" 0 America/Denver -6.0 0)
+declare -a sponsite2=(stest2 ff9d86cea1b021e9409657eb94c91860 8c4f98705d08c92f163ad3da7ece9f77 stwo@studyspot.com Spon Site2 1 1 "2013-10-18 15:17:41" "2013-10-18 15:17:41" "2013-10-18 15:17:41" 0 America/Denver -6.0 0)
+declare -a sponsite123=(stest3 ff9d86cea1b021e9409657eb94c91860 8c4f98705d08c92f163ad3da7ece9f77 s123@studyspot.com Spon Site1,2,3 1 1 "2013-10-18 15:17:41" "2013-10-18 15:17:41" "2013-10-18 15:17:41" 0 America/Denver -6.0 0)
+declare -a sponsite23=(stest4 ff9d86cea1b021e9409657eb94c91860 8c4f98705d08c92f163ad3da7ece9f77 s2and3@studyspot.com Spon Site2,3 1 1 "2013-10-18 15:17:41" "2013-10-18 15:17:41" "2013-10-18 15:17:41" 0 America/Denver -6.0 0)
 
 # RO
-declare -a rosite1=(rotest1 4afec5ea5fe0e96b86cf5c2198d8a619 7ccfb62083133d9d1abc269d3805dea1 roone@studyspot.com RO ONE 1 1 "2013-10-18 15:20:44" "2013-10-18 15:20:44" "2013-10-18 15:20:44" 0 America/Denver -6.0 0)
-declare -a rosite2=(rotest2 4afec5ea5fe0e96b86cf5c2198d8a619 7ccfb62083133d9d1abc269d3805dea1 rotwo@studyspot.com RO TWO 1 1 "2013-10-18 15:20:44" "2013-10-18 15:20:44" "2013-10-18 15:20:44" 0 America/Denver -6.0 0)
-declare -a rosite123=(rotest3 4afec5ea5fe0e96b86cf5c2198d8a619 7ccfb62083133d9d1abc269d3805dea1 ro123@studyspot.com RO 123 1 1 "2013-10-18 15:20:44" "2013-10-18 15:20:44" "2013-10-18 15:20:44" 0 America/Denver -6.0 0)
-declare -a rosite23=(rotest4 4afec5ea5fe0e96b86cf5c2198d8a619 7ccfb62083133d9d1abc269d3805dea1 ro2and3@studyspot.com RO 23 1 1 "2013-10-18 15:20:44" "2013-10-18 15:20:44" "2013-10-18 15:20:44" 0 America/Denver -6.0 0)
+declare -a rosite1=(rotest1 4afec5ea5fe0e96b86cf5c2198d8a619 7ccfb62083133d9d1abc269d3805dea1 roone@studyspot.com Ro Site1 1 1 "2013-10-18 15:20:44" "2013-10-18 15:20:44" "2013-10-18 15:20:44" 0 America/Denver -6.0 0)
+declare -a rosite2=(rotest2 4afec5ea5fe0e96b86cf5c2198d8a619 7ccfb62083133d9d1abc269d3805dea1 rotwo@studyspot.com Ro Site2 1 1 "2013-10-18 15:20:44" "2013-10-18 15:20:44" "2013-10-18 15:20:44" 0 America/Denver -6.0 0)
+declare -a rosite123=(rotest3 4afec5ea5fe0e96b86cf5c2198d8a619 7ccfb62083133d9d1abc269d3805dea1 ro123@studyspot.com Ro Site1,2,3 1 1 "2013-10-18 15:20:44" "2013-10-18 15:20:44" "2013-10-18 15:20:44" 0 America/Denver -6.0 0)
+declare -a rosite23=(rotest4 4afec5ea5fe0e96b86cf5c2198d8a619 7ccfb62083133d9d1abc269d3805dea1 ro2and3@studyspot.com Ro Site2,3 1 1 "2013-10-18 15:20:44" "2013-10-18 15:20:44" "2013-10-18 15:20:44" 0 America/Denver -6.0 0)
 
 # perm roles/misc
 
 declare -a prmroles=("Study Coordinator" Investigator "Data Manager" Monitor Sponsor "Read Only")
-declare -a prms=(sctest invtst dmtest montst spntst rontst)
+declare -a prms=(scsite invsite dmsite monsite sponsite rosite)
 declare -a unames=(scsite1 scsite2 scsite123 scsite23 invsite1 invsite2 invsite123 invsite23 dmsite1 dmsite2 dmsite123 dmsite23 monsite1 monsite2 monsite123 monsite23 sponsite1 sponsite2 sponsite123 sponsite23 rosite1 rosite2 rosite123 rosite23)
 declare -a colhdgs=(username password salt email first_name last_name email_verified active created modified last_password_change_date failed_login_attempts time_zone time_zone_offset password_reset_flag)
 
@@ -51,11 +51,28 @@ declare -a colhdgs=(username password salt email first_name last_name email_veri
 function insert_user_names() {
 	loop=0;
 	inlp=1;
+	nend=""
 	while [ $loop -lt 6 ]
 	do	
 		while [ $inlp -lt 5 ]
 		do
-			var=${prms[loop]}$inlp;
+			if [ $inlp = "1" ]; then
+				nend="1"
+			fi
+
+			if [ $inlp = "2" ]; then
+				nend="2"
+			fi
+
+			if [ $inlp = "3" ]; then
+				nend="123"
+			fi
+
+			if [ $inlp = "4" ]; then
+				nend="23"
+			fi
+
+			var=${prms[loop]}$nend;
 			#echo $var;							
 			mysql -uroot -ps\@aFe=u\$t4 -D spotv1 -e"INSERT INTO users (username) VALUES ('"${var}"' );"
 			inlp=`expr $inlp + 1`;
