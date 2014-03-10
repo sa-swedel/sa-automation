f28c8496ce92d55bc64cabac9c658c55afccc79a
sa-swedel <swedel@summitanalytical.com>
2014-03-10 15:27:27 -0700

    updated server path

--- core-crc-happypath-nomenus.rb
+++ core-crc-happypath-nomenus.rb
@@ -24,8 +24,7 @@ File.delete( "error_log.txt" ) if File.exist?( "error_log.txt" )
 
 login = 'scsite123'
 pw = '1qaz@WSX'
-#pw = '123456'
-site = 'http://54.212.41.237/'
+site = 'http://develop.trialintel.com'
 test_count = 0
 fail_count = 0
 loop = 0
@@ -45,7 +44,7 @@ today = timedate.inspect.to_s
 # start up browser
 
 headless = Headless.new
-headless.start
+#headless.start
 
 # uncomment the line corresponding to the browser against which you want to test (no arg = firefox)
 
@@ -1048,4 +1047,4 @@ puts countstr
 
 
 b.close		
-headless.destroy
+#headless.destroy
